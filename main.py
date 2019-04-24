5564dc3710e6a43190036b43f5c2b1841db04dca
Nick Romeo <romeonj@miamioh.edu>
2019-04-24 14:59:38 -0400

    added loadPRcFileData line

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from panda3d.core import *
 from panda3d.core import VBase4
 from direct.gui.DirectGui import *
 from Menu import DropDownMenu, PopupMenu
+loadPrcFileData("", "want-wx true")
 
 import wx
 
